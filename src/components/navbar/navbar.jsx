30412c01166c47626b2569889fce5f05476b3dc3
YVES10BOBO <123149216+YVES10BOBO@users.noreply.github.com>
2024-04-14 23:13:25 +0200

    Update navbar component

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './navbar.css'
-import logo from '../assets/rema.jpg'
+import logo from '../assets/template.avif'
 import cart
 
 const navbar = () => {
   return (
     <div>
-      
+      <h1></h1>
     </div>
   )
 }
